f33c3daef69059c3aaa5fa918240b213ac9e1aa2
lukas strassel <lukasstrassel@googlemail.com>
2021-12-05 21:37:10 +0100

    fix: revert change

--- packages/contract-helpers/src/index.ts
+++ packages/contract-helpers/src/index.ts
@@ -3,8 +3,8 @@ export {
   PERMISSION,
   PERMISSION_MAP,
 } from './permissions-manager/types/PermissionManagerTypes';
-// export * from './v3-UiIncentiveDataProvider-contract';
-export * from './ui-incentive-data-provider';
+export * from './v3-UiIncentiveDataProvider-contract';
+// export * from './ui-incentive-data-provider';
 export * from './v3-UiPoolDataProvider-contract';
 export * from './wallet-balance-provider';
 export * from './cl-feed-registry';
